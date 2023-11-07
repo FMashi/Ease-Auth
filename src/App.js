3cbae8b2a2bd5312ed7f526cf731e789f08fa743
Fahad Mashi <Fahadmashi@hotmail.com>
2023-11-08 02:24:54 +0300

    U -

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
+import {
+  Home,
+  Login,
+  // Signup,
+  // PasswordChange,
+  // PasswordReset,
+  // PasswordResetConfirm,
+  // EmailVerification,
+} from "./pages/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Layout>
+      <Routes>
+        <Route path="/" Component={Home}></Route>
+        <Route path="Login/" Component={Login}></Route>
+        <Route path="signup/" Component={Signup}></Route>
+        <Route path="change/password/" Component={PasswordChange}></Route>
+        <Route path="reset/password/" Component={PasswordReset}></Route>
+        <Route
+          path="api/auth/password/reset/confirm/:uid/:token"
+          Component={PasswordResetConfirm}
+        ></Route>
+        <Route
+          path="/api/auth/registration/verify-email/:key"
+          Component={EmailVerification}
+        ></Route>
+      </Routes>
+    </Layout>
+  </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
+import './assets/css/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { Provider } from "react-redux";
+import Store from "./Store";
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={Store}>
     <App />
-  </React.StrictMode>
-);
+    </Provider>
+  </React.StrictMode>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
